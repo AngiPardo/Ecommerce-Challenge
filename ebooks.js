835434e6eaa341bd603702295cd82ae4a1db60f8
Angi Pardo <angi.pardo@correounivalle.edu.co>
2022-07-19 12:11:51 -0500

    Feat: Dinamiza la página con JS

--- ebooks.js
+++ ebooks.js
@@ -1,82 +1,87 @@
-// const jsonData = require('./ebooks.json'); // No reconoce el método require
-// console.log(jsonData);
+(() => {
+  //Seleccionado la etiqueta ul para tratarla más adelante
+const miLista = document.querySelector('ul');
 
-fetch('./ebooks.json')
-.then(response => {
-    return response.json();
-})
-.then(jsondata => console.log(jsondata));
+//Accediendo a los datos del JSON
+const miSolicitud = new Request('ebooks.json');
 
-var boton1 = document.getElementById("DescargarWhatsApp");
-var boton2 = document.getElementById("DescargarGmail");
-var boton3 = document.getElementById("DescargarIFTTT");
-var boton4 = document.getElementById("DescargarTelegram");
-var boton5 = document.getElementById("DescargarRss");
-var boton6 = document.getElementById("DescargarLifehacks");
-var boton7 = document.getElementById("DescargarAutomatizate");
-var boton8 = document.getElementById("DescargarEmprendedor");
-var boton9 = document.getElementById("DescargarEmail");
-var boton10 = document.getElementById("DescargarNinos");
-var boton11 = document.getElementById("DescargarSalidas");
-var boton12 = document.getElementById("DescargarTrucos");
+fetch(miSolicitud)
+  .then(response => response.json())
+  .then(datosObtenidosJSON => {
+    for (const ebook of datosObtenidosJSON.ebooks) {
 
-boton1.addEventListener("click",IrALibro1);
-boton2.addEventListener("click",IrALibro2);
-boton3.addEventListener("click",IrALibro3);
-boton4.addEventListener("click",IrALibro4);
-boton5.addEventListener("click",IrALibro5);
-boton6.addEventListener("click",IrALibro6);
-boton7.addEventListener("click",IrALibro7);
-boton8.addEventListener("click",IrALibro8);
-boton9.addEventListener("click",IrALibro9);
-boton10.addEventListener("click",IrALibro10);
-boton11.addEventListener("click",IrALibro11);
-boton12.addEventListener("click",IrALibro12);
+      const listItem = document.createElement('li');
+      listItem.classList.add('productoListado');
 
-function IrALibro1 () {
-    window.open("https://biz.payulatam.com/L0d6ed88570CC44");
-}
+      const nombreDelEbook = document.createElement('img');
+      nombreDelEbook.textContent = ebook.Nombre;
 
-function IrALibro2 () {
-    window.open("https://irreverente.net/InfoP/SACALE JUGO A TU CUENTA DE GMAIL.pdf");
-}
+      const precioDelEbook = document.createElement('p')
+      precioDelEbook.textContent = ebook.Precio;
 
-function IrALibro3 () {
-    window.open("https://biz.payulatam.com/L0d6ed87049F19F");
-}
+      const linkDeDescarga = document.createElement('input');
+      linkDeDescarga.textContent = ebook.enlaceDelProducto;
 
-function IrALibro4 () {
-    window.open("https://biz.payulatam.com/L0d6ed88541D181");
-}
+      const URLImage = ebook.imagenDelProducto;
+      const textAlt = ebook.textoAlt.toLowerCase();
+      const id = ebook.id;
 
-function IrALibro5 () {
-    window.open("https://irreverente.net/InfoP/COMO AHORRAR TIEMPO EN Instagram Facebook y TikTok.pdf");
-}
+      listItem.innerHTML = `<img class="imagen_ebook" src="${ebook.imagenDelProducto}" alt="${ebook.textoAlt}">
+      <p class="producto-descripcion">${ebook.Precio}</p>
+      <input type="button" value="Descargar" id="${ebook.id}" class="botonDescargar">`;
+      
+      miLista.appendChild(listItem);
 
-function IrALibro6 () {
-    window.open("https://irreverente.net/InfoP/AHORRA TIEMPO Y DINERO Usando Apps y LifeHacks para Todos.pdf");
-}
+      const botonDeDescarga = document.getElementById(`${ebook.id}`);
+      botonDeDescarga.addEventListener('click',IrALibro);
 
-function IrALibro7 () {
-    window.open("https://irreverente.net/InfoP/AHORRA TIEMPO Y DINERO automatizando tus procesos.pdf");
-}
+      const URLParaIrAlLibro = `${ebook.enlaceDelProducto}`;
+      function IrALibro() {
+        window.open(URLParaIrAlLibro);
+      }
 
-function IrALibro8 () {
-    window.open("https://irreverente.net/InfoP/AHORRA TIEMPO Y DINERO Usando Apps y LifeHacks para Emprendedores.pdf");
-}
+      //Validación de la búsqueda en el sitio (FALTA COMPLETAR)
 
-function IrALibro9 () {
-    window.open("https://irreverente.net/InfoP/COMO LOGRAR QUE TU CORREO ELECTRONICO TRABAJE PARA TI.pdf");
-}
+      var cajaDeBusquedaPagina = document.getElementById("cajaDeBusqueda");
+      cajaDeBusquedaPagina.addEventListener('click',refrescarPagina);
 
-function IrALibro10 () {
-    window.open("https://irreverente.net/InfoP/SALIDAS CON NIÑOS EN LA CIUDAD Apps y LifeHacks de Ahorro.pdf");
-}
+      function refrescarPagina() {
+        if (cajaDeBusqueda.value!="") {
+          location.reload();
+        }
+      }
 
-function IrALibro11 () {
-    window.open("https://irreverente.net/InfoP/SALIR CON AMIGOS O EN PAREJA LifeHacks con Apps para Ahorrar Dinero y Tiempo.pdf");
-}
+      var botonBarraDeBusquedaPagina = document.getElementById("botonBarraDeBusqueda");
+      botonBarraDeBusquedaPagina.addEventListener("click",buscarLoSolicitado);
+      
+      function buscarLoSolicitado(event) {
+        event.preventDefault();
+        if (cajaDeBusqueda.value != "") {
+          var criterioDeBusqueda = cajaDeBusqueda.value.toLowerCase();
+          if (textAlt.includes(criterioDeBusqueda)==false){
+            miLista.removeChild(listItem);
+          }
 
-function IrALibro12 () {
-    window.open("https://irreverente.net/InfoP/TRUCOS MODERNOS para gastar menos dinero sin sacrificar confort.pdf");
-}
+          const botonDeDescarga = document.getElementById(`${ebook.id}`);
+          botonDeDescarga.addEventListener('click',IrALibro);
+
+          const URLParaIrAlLibro = `${ebook.enlaceDelProducto}`;
+          function IrALibro() {
+            if(cajaDeBusqueda.value != "") {
+              if (listItem.length!=0){
+                window.open(URLParaIrAlLibro);
+              } else {
+                alert("Sin coincidencias");
+              }
+            } else {
+              alert("Digita un criterio");
+            }
+            
+          }
+        }
+      }
+    }
+  })
+  .catch(console.error);
+  
+})();
--- validacion.js
+++ validacion.js
@@ -1,38 +1,24 @@
-// //Validación del formulario de contacto (FUNCIONA)
-// var botonEnviarFormulario = document.getElementById("botonEnviarAhoraElFormulario");
-// botonEnviarFormulario.addEventListener("click",enviarFormulario)
+//Validación del formulario de contacto (FUNCIONA)
+var botonEnviarFormulario = document.getElementById("botonEnviarAhoraElFormulario");
+botonEnviarFormulario.addEventListener("click",enviarFormulario)
 
-// var nombre = document.getElementById("nombre");
+var nombre = document.getElementById("nombre");
 
-// var mensaje = document.getElementById("mensaje");
+var mensaje = document.getElementById("mensaje");
 
-// function enviarFormulario () {
-//     var nombreAEnviar = nombre.value;
-//     var mensajeAEnviar = mensaje.value;
-//     var stringFinalaEnviar = "mailto:angi.pardo@irreverente.net?subject=" + "Mensaje enviado desde la tienda virtual por: " + nombreAEnviar + "&body=" + mensajeAEnviar;
-//     window.open(stringFinalaEnviar);
-// }
+function enviarFormulario () {
+    var nombreAEnviar = nombre.value;
+    var mensajeAEnviar = mensaje.value;
+    var stringFinalaEnviar = "mailto:angi.pardo@irreverente.net?subject=" + "Mensaje enviado desde la tienda virtual por: " + nombreAEnviar + "&body=" + mensajeAEnviar;
+    window.open(stringFinalaEnviar);
+}
 
-// //Validación de la búsqueda en el sitio (FALTA COMPLETAR)
-
-// var botonBarraDeBusquedaPagina = document.getElementById("botonBarraDeBusqueda");
-// botonBarraDeBusquedaPagina.addEventListener("click",buscarLoSolicitado);
-
-// function buscarLoSolicitado() {
-//     if (cajaDeBusqueda.value != "") {
-//         var criterioDeBusqueda = cajaDeBusqueda.value;
-//         // alert(criterioDeBusqueda); completar el proceso de búsqueda por palabra clave en la página
-//     } else {
-//         alert("Por favor ingresa un criterio de búsqueda en la caja");
-//     }
-// }
-
-//Validación del login a la tienda (COMPLETAR EL PROCESO CON EL BACKEND O ACCESO INTERNO)
+//Validación del login a la tienda (BackEnd simulado para efectos del desafío)
 
 var botonLoginTienda = document.getElementById("botonLogin");
 botonLoginTienda.addEventListener("click",ingresarInternamente);
 
-// Para efectos de la demostración objetivo del challenge, los usuarios y las contraseñas aceptadas son "Angi" con "XXXX" y "Steven" con "ZZZZ"
+// Para efectos de la demostración objetivo del challenge, el usuario y la contraseña aceptada es "Angi" con "XXXX"
 
 var usuario = document.getElementById("cajaDeUsuario");
 var contrasena = document.getElementById("cajaDeContrasena");
@@ -43,22 +29,12 @@ function ingresarInternamente(usuarioAComprobar,contrasenaAComprobar){
     var contrasenaAComprobar = contrasena.value;
 
     if (usuarioAComprobar == "Angi") {
-        if (contrasenaAComprobar == "XXXX"){
-            alert("Ingresando");
-        } else {
-            alert("Usuario y/o contraseña incorrectos");
-        }  
-    } else if (usuarioAComprobar == "") {
-        alert("Datos inválidos o incompletos");
-    }
-    
-    if (usuarioAComprobar == "Steven") {
-        if (contrasenaAComprobar == "ZZZZ"){
-            alert("Ingresando");
+        if (contrasenaAComprobar == "XXXX") {
+            alert("Datos correctos");
         } else {
             alert("Usuario y/o contraseña incorrectos");
         }
-    } else if (usuarioAComprobar == "") {
-        alert("Datos inválidos o incompletos");
+    } else {
+        alert("Usuario y/o contraseña incorrectos");
     }
 }
